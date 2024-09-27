6efa15e5a5f47823c567f5bf7726b23494a17558
Felipe Vasconcelos <felipe.vasconcelos-ext@ab-inbev.com>
2024-09-26 21:57:24 -0300

    fixing client

--- boletoApi-master/src/main/java/com/example/boleto/BoletoApi/client/ContaFeignClient.java
+++ boletoApi-master/src/main/java/com/example/boleto/BoletoApi/client/ContaFeignClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "conta", url = "http://localhost:8762/MS-CONTA/contas/pagarBoleto")
+@FeignClient(name = "conta", url = "http://localhost:8081/contas")
 public interface ContaFeignClient {
 
-    @PostMapping("/{idConta}/{valorBoleto}")
+    @PostMapping("/pagarBoleto/{idConta}/{valorBoleto}")
     void pagarBoleto(@PathVariable("idConta") Long idConta, @PathVariable("valorBoleto") Double valorBoleto);
 }
--- transferencia/src/main/java/com/example/transferencia/client/ContaClient.java
+++ transferencia/src/main/java/com/example/transferencia/client/ContaClient.java
@@ -4,10 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "conta", url = "http://localhost:8762/MS-CONTA/contas/transferir")
+@FeignClient(name = "conta", url = "http://localhost:8081/contas")
 public interface ContaClient {
 
-    @PostMapping("/{contaOrigem}/{valor}/{contaDestino}")
+    @PostMapping("/transferir/{contaOrigem}/{valor}/{contaDestino}")
     void operacaoTransferencia(@PathVariable("contaOrigem") int contaOrigem, @PathVariable("valor") Double valor,
                                @PathVariable("contaDestino") int contaDestino);
 }
